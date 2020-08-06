9a1e4ef719d6f57f038aedbf519a734607e9cae1
Daniel Rendon <rendod99@gmail.com>
2020-08-06 09:43:45 -0500

    added a form to html page and made it interactive with the JS page.

---
+++ PassGenHW3/Develop/script.js
@@ -0,0 +1,82 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+//deactivte d-none
+var passCard = document.querySelector("#passCard");
+
+if (passCard.classList.contains("d-none")) {
+  passCard.classList.remove("d-none");
+
+} else {
+  passCard.classList.add("d-none");
+} 
+
+
+
+
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+//GIVEN I need a new, secure password
+//WHEN I click the button to generate a password
+//THEN I am presented with a series of prompts for password criteria
+
+//WHEN prompted for password criteria
+//THEN I select which criteria to include in the password
+
+//WHEN prompted for the length of the password
+//THEN I choose a length of at least 8 characters and no more than 128 characters
+
+//WHEN prompted for character types to include in the password
+//THEN I choose lowercase, uppercase, numeric, and/or special characters
+
+//WHEN I answer each prompt
+//THEN my input should be validated and at least one character type should be selected
+
+//WHEN all prompts are answered
+//THEN a password is generated that matches the selected criteria
+
+//WHEN the password is generated
+//THEN the password is either displayed in an alert or written to the page
+
+// check boxes : lower case, upper case, numeric, special  charactors, length.
+
+const randomFunction = { 
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol
+};
+
+
+
+
+
+function getRandomLower(){
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+function getRandomUpper(){
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+function getRandomNumber(){
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+function getRandomSymbol(){
+  const symbols = "!@#$%^&*(){}[]=<>/,."
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+console.log(getRandomSymbol());
